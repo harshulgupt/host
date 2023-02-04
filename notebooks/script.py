10cac8d6eeafd13ba07a8d3238d32d0307ae27af
Harshul Gupta <51258362+harshulgupt@users.noreply.github.com>
2023-02-04 18:09:49 +0530

    Update script.py

--- notebooks/script.py
+++ notebooks/script.py
@@ -1,20 +1,8 @@
-import os
-import requests
+import os  # importing os module
 
-# retrieve token from environment variable
-token = os.getenv("PAT")
+# defining Env variable with 'HOME' as value
+key = "LAL"
 
-# fetch data from private repository using GitHub API
-url = "https://api.github.com/repos/<user>/json_pvt/data.txt"
-headers = {
-    "Authorization": "Token " + token
-}
-response = requests.get(url, headers=headers)
-if response.status_code == 200:
-    data = response.json()["content"]
-else:
-    raise Exception("Could not fetch data from private repository")
-
-# write data to output.txt file
-with open("output.txt", "w") as f:
-    f.write(data)
+# invoking getenv() method
+value = os.getenv(key, default=None)
+print(f"Value of env variable key='LAL': {value}")
